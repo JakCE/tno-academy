3e39fcc51683717b5a991182257e72e48a664fe0
JakCE <jak.ce30@gmail.com>
2026-02-11 20:33:23 -0500

    mejora seo and performance

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,18 @@ import tailwindcss from "@tailwindcss/vite";
 
 import icon from "astro-icon";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://tno-academy.vercel.app",
   vite: {
       plugins: [tailwindcss()],
   },
-
-  integrations: [icon()],
+  integrations: [icon(), sitemap()],
+  output: "static",
+  compressHTML: true,
+  build: {
+    inlineStylesheets: "auto"
+  }
 });
